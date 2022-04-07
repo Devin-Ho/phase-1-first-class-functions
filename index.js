c09837fefc825335bea0b08657de56bcfd098214
Devin Ho <djxho36@gmail.com>
2022-04-07 18:08:25 -0400

    commit tree 344f2bd4093aafc032b59f9d28a2a25522cf2219

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(spy){
+    spy();
+}
+
+function returnsANamedFunction(){
+    return function returnsANamedFunction(){};
+
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){}; 
+
+
+}
